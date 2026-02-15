3a3f91a90009e9cee1888db83a6151b18448637d
niranjan <niranjan@gmail.com>
2026-02-16 00:12:22 +0530

    Updated main.py - fixed issue

--- website/db.py
+++ website/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://parmarniranjan15_db_user:Stylo1234@cluster0.ngtydeh.mongodb.net/stylo_db?retryWrites=true&w=majority")
 db = client["stylo_db"]
 
 products = db["products"]
